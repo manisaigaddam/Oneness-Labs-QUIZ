e49b3295370212d4ef2b87f6951fe025ac87185d
manisaigaddam <68413467+manisaigaddam@users.noreply.github.com>
2024-05-13 16:59:31 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,116 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+        numb: 1,
+        question: "Who is the CEO of Oneness Labs?",
+        answer: "Kathy Gong",
+        options: [
+            "Kathy Gong",
+            "0xWest",
+            "Tao",
+            "Dimlak"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What is the Oneness Matrix?",
+        answer: "An operating system for the gaming universe",
+        options: [
+            "A game developed by Oneness Labs",
+            "An operating system for the gaming universe",
+            "A blockchain protocol",
+            "A virtual reality headset"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What is the primary goal of Oneness Labs?",
+        answer: "All of the above",
+        options: [
+            "To integrate traditional gaming with the blockchain ecosystem",
+            "To create the world's first gaming-intent layer",
+            "To facilitate widespread acceptance of Web3 gaming",
+            "All of the above"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What is the Oneness IPverse?",
+        answer: "A transformative initiative around game intellectual properties",
+        options: [
+            "A blockchain protocol",
+            "A transformative initiative around game intellectual properties",
+            "A virtual reality headset",
+            "A social media platform"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What is One Spark?",
+        answer: "The cornerstone NFT within the Oneness Labs ecosystem",
+        options: [
+            "A game developed by Oneness Labs",
+            "A virtual reality headset",
+            "A blockchain protocol",
+            "The cornerstone NFT within the Oneness Labs ecosystem"
+        ]
+    },
+    {
+        numb: 6,
+        question: "Where does the adventure take place in the Oneness Matrix?",
+        answer: "In the enchanting city of Onar",
+        options: [
+            "In the dark realms of the internet",
+            "In the bustling streets of New York City",
+            "In the enchanted forests of Avalon",
+            "In the enchanting city of Onar"
+        ]
+    },
+    {
+        numb: 7,
+        question: "What is the supply of One Spark NFTs?",
+        answer: "1,000",
+        options: [
+            "100",
+            "1,000",
+            "10,000",
+            "100,000"
+        ]
+    },
+    {
+        numb: 8,
+        question: "What is the mint date for One Spark?",
+        answer: "May 2024",
+        options: [
+            "March 2024",
+            "April 2024",
+            "May 2024",
+            "June 2024"
+        ]
+    },
+    {
+        numb: 9,
+        question: "What network is used for One Spark minting?",
+        answer: "BTC",
+        options: [
+            "ETH",
+            "BTC",
+            "BSC",
+            "ADA"
+        ]
+    },
+    {
+        numb: 10,
+        question: "What is the goal of Oneness Labs?",
+        answer: "To migrate the $200 billion traditional gaming industry to Web3",
+        options: [
+            "To become the biggest gaming company in the world",
+            "To dominate the cryptocurrency market",
+            "To migrate the $200 billion traditional gaming industry to Web3",
+            "To create the first fully decentralized virtual world"
+        ]
+    }
+    // you can uncomment the below codes and make duplicate as more as you want to add question
+    // but remember you need to give the numb value serialize like 1,2,3,4,5,6,7,8,9.....
+  ];
+  
